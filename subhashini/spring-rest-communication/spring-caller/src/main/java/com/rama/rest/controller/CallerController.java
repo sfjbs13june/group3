e26aca993555da3635049dd82fdd2f1e0de33bc0
sfjbs <sfjbs13june@gmail.com>
2022-06-24 06:39:52 +0000

    Communication

---
+++ subhashini/spring-rest-communication/spring-caller/src/main/java/com/rama/rest/controller/CallerController.java
@@ -0,0 +1,36 @@
+package com.rama.rest.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CallerController {
+  private static final Logger LOGGER = LoggerFactory.getLogger(CallerController.class);
+  @Autowired
+  private RestTemplate restTemplate;
+  @Value("${receive.url}")
+  String receiveUrl; //http://localhost:10082/receiver/get
+
+  @GetMapping("/caller/get")
+  public String getDetail() {
+    ResponseEntity<String> response
+      = restTemplate.getForEntity(receiveUrl, String.class);
+    LOGGER.info(response.getStatusCode().toString());
+    LOGGER.info(response.getBody());
+    return "caller receive data:"+response.getBody();
+  }
+
+  @GetMapping("/caller/hello")
+  public String getHello() {
+    ResponseEntity<String> response = restTemplate.getForEntity(receiveUrl, String.class);
+    LOGGER.info(response.getStatusCode().toString());
+    LOGGER.info(response.getBody());
+    return "caller received hello:"+response.getBody();
+  }
+}
